cd27a07ea4c39a60a08ede993e7ab75d1d7c9766
RaulWei <mingwei.wei@foxmail.com>
2018-05-22 23:19:49 +0800

    refs #2 init smart contract for children-say

--- smart-contract/children-say-contract.js
+++ smart-contract/children-say-contract.js
@@ -5,9 +5,13 @@
 var ChildrenSayItem = function (text) {
     if (text) {
         var obj = JSON.parse(text);
-        // todo
+        this.date = obj.date;
+        this.content = obj.content;
+        this.author = obj.author;
     } else {
-        // todo
+        this.date = "";
+        this.content = "";
+        this.author = "";
     }
 };
 
@@ -20,10 +24,11 @@ ChildrenSayItem.prototype = {
 // core actions of children-say
 
 var ChildrenSay = function () {
+    LocalContractStorage.defineProperty(this, "count");
     LocalContractStorage.defineMapProperty(this, "walletContents");
     LocalContractStorage.defineMapProperty(this, "essenceContents", {
         parse: function (text) {
-            // todo
+            return new ChildrenSayItem(text);
         },
         stringify: function (o) {
             return o.toString();
@@ -34,10 +39,57 @@ var ChildrenSay = function () {
 ChildrenSay.prototype = {
 
     init: function () {
-        // todo
-    }
+        this.count = new BigNumber(0);
+    },
 
-    // todo
+    total: function() {
+        return new BigNumber(this.count).toNumber();
+    },
+
+    add: function(author, date, content) {
+        var from = author.trim() || Blockchain.transaction.from;
+        if (!from) {
+            throw new Error("Empty key when add children-say.");
+        }
+        date = date.trim();
+        content = content.trim();
+        if (!date || !content) {
+            throw new Error("Empty date or content when add children-say");
+        }
+
+        var childrenSayItem = new ChildrenSayItem();
+        childrenSayItem.date = date;
+        childrenSayItem.content = content;
+        childrenSayItem.author = from;
+
+        var index = this.count;
+        this.essenceContents.put(index, childrenSayItem);
+        var walletContents = this.walletContents.get(from) || [];
+        walletContents.put(index);
+        this.walletContents.put(from, walletContents);
+
+        this.count = new BigNumber(index).plus(1);
+    },
+
+    get: function(author) {
+        var from = author.trim() || Blockchain.transaction.from;
+        if (!from) {
+            throw new Error("Empty key when get children-say.");
+        }
+        var walletContents = this.walletContents.get(from);
+        if (!walletContents) {
+            throw new Error("Failed to get contents from wallet = ${from}.");
+        }
+        var res = [];
+        for (var i = 0; i < walletContents.length; i++) {
+            var idx = walletContents[i];
+            var content = this.essenceContents.get(idx);
+            if (content) {
+                res.push(content);
+            }
+        }
+        return res;
+    }
 
 };
 
